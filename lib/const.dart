6edd27e7eb83e64cd26b1de5e69c82b582e6c398
Nguyen Anh Nhat <mauxanhduong2472002@gmail.com>
2024-04-21 09:53:49 +0700

    Revert "comit"
    This reverts commit 82d4bf7d52c9099c12b0d972fa7c4fe36e0123ae.

--- lib/const.dart
+++ lib/const.dart
@@ -8,32 +8,3 @@ const Color color_4 =Color.fromARGB(255, 149, 45, 55);
 const Color color_5 = Color.fromARGB(255, 198, 198, 198);
 const Color primarys = Color(0xFFCFD5F3);
 
-const Color color_background = Color(0xFFC21010);
-TextStyle UNtextstyle(double size) {
-  return TextStyle(
-    color: color_background,
-    fontWeight: FontWeight.bold,
-    fontSize: size,
-  );
-}
-
-//Khung nhập dữ liệu input
-  TextFormField textFormField(String label, IconData icon) {
-    return TextFormField(
-      decoration: InputDecoration(
-          label: Text(label),
-          prefixIcon: Icon(icon),
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
-          floatingLabelStyle: TextStyle(
-            color: color_background,
-          ),
-          floatingLabelBehavior: FloatingLabelBehavior.auto,
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              width: 2,
-              color: color_background,
-            ),
-            borderRadius: BorderRadius.circular(15),
-          )),
-    );
-  }
